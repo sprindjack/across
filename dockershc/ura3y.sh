aac7b61c5c1b16c25fd949a9f6c1309a43b8e22a
sprindjack <sprindjack@hotmail.com>
2020-11-10 11:55:29 +0800

    Update ura3y.sh

--- dockershc/ura3y.sh
+++ dockershc/ura3y.sh
@@ -12,7 +12,7 @@ if [[ ! -f "/workerone" ]]; then
         {
             "port": "8080","listen": "0.0.0.0","protocol": "vless",
             "settings": {"clients": [{"id": "8f91b6a0-e8ee-11ea-adc1-0242ac120002"}],"decryption": "none"},
-            "streamSettings": {"network": "ws","wsSettings": {"path": "/pic"}}
+            "streamSettings": {"network": "ws","wsSettings": {"path": "pic"}}
         }
     ],
     "outbounds": 
